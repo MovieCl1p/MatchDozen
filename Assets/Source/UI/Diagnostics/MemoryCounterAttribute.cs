c1ce555dd4deaf1f3c27de68849279de4af23b4f
AntonAmerov <antonametov88@gmail.com>
2017-06-22 21:08:15 +0300

    fix memory attribute

--- Assets/Source/UI/Diagnostics/MemoryCounterAttribute.cs
+++ Assets/Source/UI/Diagnostics/MemoryCounterAttribute.cs
@@ -20,12 +20,12 @@ namespace UI.Diagnostics
 		
 		public override string ToString ()
 		{
-#if UNITY_IPHONE
-			return EtceteraBinding.getMemoryStatistic();
-#else			
+//#if UNITY_IPHONE
+			//return EtceteraBinding.getMemoryStatistic();
+//#else			
 			return string.Format("{0}",
 			                     System.GC.GetTotalMemory(true) / 1024 / 1024);
-#endif			
+//#endif			
 		}
 	}
 }
